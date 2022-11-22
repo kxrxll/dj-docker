9eaa1d4ef9bb1c03b8a5b1e980413035178437f3
kxrxll <kovalyovkirill@gmail.com>
2022-11-22 22:52:16 +0300

    done

---
+++ logistic/serializers.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from logistic.models import Product, Stock, StockProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['quantity', 'price', 'product']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    def validate(self, attrs):
+        if not attrs['positions'][0]:
+            raise ValidationError('Список пуст!')
+        return attrs
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+        for position in positions:
+            stocks_products = StockProduct(
+                stock=stock,
+                product=position['product'],
+                quantity=position['quantity'],
+                price=position['price']
+            )
+            stocks_products.save()
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            stocks_products = StockProduct(
+                stock=stock,
+                product=position['product'],
+                quantity=position['quantity'],
+                price=position['price']
+            )
+            stocks_products.save()
+        return stock
