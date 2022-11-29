75b8c94b683eb88b34a60426edd72768c27b02c7
kxrxll <kovalyovkirill@gmail.com>
2022-11-29 20:13:37 +0300

    done with feedback once again

--- logistic/serializers.py
+++ logistic/serializers.py
@@ -43,11 +43,12 @@ class StockSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         positions_data = validated_data.pop('positions')
         positions = instance.positions.all()
-        positions = list(positions)
         stock = super().update(instance, validated_data)
         for position in positions_data:
-            item = positions.pop(0)
-            item.quantity = position.get('quantity', item.quantity)
-            item.price = position.get('price', item.price)
-            item.save()
+            StockProduct.objects.update_or_create(
+                stock=instance,
+                product=position['product'],
+                quantity=position['quantity'],
+                price=position['price']
+            )
         return stock
